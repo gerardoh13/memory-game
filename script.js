51ff6884742a22c5d4d2964fa92af9ce6240cf9a
Gerardo <gerardoh13@gmail.com>
2022-07-28 07:51:23 -0700

    added option to chose cards played

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const gameContainer = document.getElementById("game");
-
+const score = document.getElementById("score")
+document.body.style.backgroundColor = 'rgb(15, 93, 45)';
 const cardBack = 'back-of-card.jpg'
 
 const cards = [
@@ -7,35 +8,54 @@ const cards = [
   id: 'AC'}, 
   {src: "https://deckofcardsapi.com/static/img/KD.png",
   id: 'KD'}, 
-  {src: "https://deckofcardsapi.com/static/img/AD.png",
-  id: 'AD'}, 
+  {src: "https://deckofcardsapi.com/static/img/AH.png",
+  id: 'AH'}, 
   {src: "https://deckofcardsapi.com/static/img/KS.png",
   id: 'KS'}, 
+  {src: "https://deckofcardsapi.com/static/img/QC.png",
+  id: 'QC'}, 
+  {src: "https://deckofcardsapi.com/static/img/QD.png",
+  id: 'QD'}, 
+  {src: "https://deckofcardsapi.com/static/img/JH.png",
+  id: 'JH'}, 
+  {src: "https://deckofcardsapi.com/static/img/JS.png",
+  id: 'JS'}, 
+  {src: "https://deckofcardsapi.com/static/img/0C.png",
+  id: '0C'}, 
+  {src: "https://deckofcardsapi.com/static/img/0D.png",
+  id: '0D'}, 
+  {src: "https://deckofcardsapi.com/static/img/9S.png",
+  id: '9S'},
+  {src: "https://deckofcardsapi.com/static/img/9C.png",
+  id: '9C'}, 
 ]
+
+// let cardVals = ['A','K','Q','J','2','3','4','5','6','7','8','9','0']
+// let suits = ['C','H','D','S']
+
+// loopedCards = []
+
+// let suitIndex = 0
+// for (let i = 0; i < 13; i++){
+//   suitIndex++
+//   suitIndex = i % 4 === 0 ? 0 : suitIndex
+//   loopedCards.push({
+//     src: `https://deckofcardsapi.com/static/img/${cardVals[i]}${suits[suitIndex]}.png`,
+//     id:  `${cardVals[i]}${suits[suitIndex]}`
+//   })
+// }
 
 let clickedCards = []
 let matchCount = 0
+let cardFlips = 0
+let cardsPlayed = 0
 
-//Get a new deck of 20 cards
-// fetch('http://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,AC,KS,KC,QH,QD,JH,JD,0S,0C,9S,9C,8H,8D,7H,7D,6S,6C,5H,5D')
-//     .then(response => response.json())
-//     .then(data => apiResponse = data);
 
-// here is a helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want ot research more
 function shuffle(array) {
   let counter = array.length;
-
-  // While there are elements in the array
   while (counter > 0) {
-    // Pick a random index
     let index = Math.floor(Math.random() * counter);
-
-    // Decrease counter by 1
     counter--;
-
-    // And swap the last element with it
     let temp = array[counter];
     array[counter] = array[index];
     array[index] = temp;
@@ -44,36 +64,21 @@ function shuffle(array) {
   return array;
 }
 
-let shuffledCards = shuffle(cards);
-
-// this function loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
 function createCards(cardArray) {
   for (let card of cardArray) {
-    // create a new div
-    // const newDiv = document.createElement("div");
     const newImg = document.createElement("img")
     newImg.setAttribute("src", cardBack);
-    // give it a class attribute for the value we are looping over
-    newImg.classList.add("card");
+    newImg.classList.add("cards");
     newImg.id = card.id
-
-    // call a function handleCardClick when a div is clicked on
     newImg.addEventListener("click", handleCardClick);
-    // newDiv.append(newImg)
-    // append the div to the element with an id of game
     gameContainer.append(newImg);
   }
 }
 function checkMatches () {
   let cardOne = clickedCards[0]
   let cardTwo = clickedCards[1]
-  console.log(cardOne)
-  console.log(cardTwo)
   if (cardOne.id[0] === cardTwo.id[0]){
     matchCount++
-    alert(`You have ${matchCount} matches`)
   }  else {
     cardOne.src = cardBack
     cardOne.addEventListener("click", handleCardClick);
@@ -81,28 +86,49 @@ function checkMatches () {
     cardTwo.addEventListener("click", handleCardClick);
   }
   clickedCards = []
+  if (matchCount === cardsPlayed / 2){
+    alert('Congrats! You matched all the cards!')
+  }
+}
+function updateScore(){
+  cardFlips++
+  score.innerText = `Cards flipped: ${cardFlips}`
 
 }
 
-// TODO: Implement this function!
 function handleCardClick(event) {
-  // you can use event.target to see which element was clicked
+  console.log('clicked')
   if (clickedCards.length === 2){
-    console.log('too many')
+    return
   }
   else {
     const found = cards.find(element => element.id === event.target.id);
     event.target.src = found.src
     event.target.removeEventListener("click", handleCardClick);
     clickedCards.push(event.target)
+    updateScore()
     if (clickedCards.length === 2){
       setTimeout(checkMatches, 1000)  
     }
   }
-
 }
 
-// when the DOM loads
-createCards(shuffledCards);
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
 
-/* */
+function newGame(num){
+  const oldCards = document.getElementsByClassName("cards");
+  if (oldCards.length !== 0){
+    matchCount = 0
+    cardFlips = 0
+    score.innerText = "Cards flipped: 0"
+    removeAllChildNodes(gameContainer)
+  }
+  cardsPlayed = num
+  let arr = cards.slice(0, num)
+  let shuffledCards = shuffle(arr);
+  createCards(shuffledCards);
+}
