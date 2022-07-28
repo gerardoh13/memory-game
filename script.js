73c33b10d8d62b932e253bdc0b3c1130f8b56d2a
Gerardo Huerta <gerardoh13@gmail.com>
2022-07-28 16:32:20 -0700

    Added CSS, score and timer

--- script.js
+++ script.js
@@ -1,55 +1,73 @@
 const gameContainer = document.getElementById("game");
-const score = document.getElementById("score")
+const scoreDisplay = document.getElementById("score")
+const timerDisplay = document.getElementById("timer")
 document.body.style.backgroundColor = 'rgb(15, 93, 45)';
-const cardBack = 'back-of-card.jpg'
+const cardBack = "back-of-card.jpg"
 
-const cards = [
-  {src: "https://deckofcardsapi.com/static/img/AC.png",
-  id: 'AC'}, 
-  {src: "https://deckofcardsapi.com/static/img/KD.png",
-  id: 'KD'}, 
-  {src: "https://deckofcardsapi.com/static/img/AH.png",
-  id: 'AH'}, 
-  {src: "https://deckofcardsapi.com/static/img/KS.png",
-  id: 'KS'}, 
-  {src: "https://deckofcardsapi.com/static/img/QC.png",
-  id: 'QC'}, 
-  {src: "https://deckofcardsapi.com/static/img/QD.png",
-  id: 'QD'}, 
-  {src: "https://deckofcardsapi.com/static/img/JH.png",
-  id: 'JH'}, 
-  {src: "https://deckofcardsapi.com/static/img/JS.png",
-  id: 'JS'}, 
-  {src: "https://deckofcardsapi.com/static/img/0C.png",
-  id: '0C'}, 
-  {src: "https://deckofcardsapi.com/static/img/0D.png",
-  id: '0D'}, 
-  {src: "https://deckofcardsapi.com/static/img/9S.png",
-  id: '9S'},
-  {src: "https://deckofcardsapi.com/static/img/9C.png",
-  id: '9C'}, 
-]
+//generate an array of 26 card objects with img src and id. I used this to avoid having a long array in my js file
+const cardVals = ['A','A','K','K','Q','Q','J','J','2','2','3','3','4','4','5','5','6','6','7','7','8','8','9','9','0','0']
+const suits = ['C','H','D','S']
+let cards = []
+let suitIndex = 0
+for (let i = 0; i < cardVals.length; i++){
+  suitIndex++
+  suitIndex = i % 4 === 0 ? 0 : suitIndex
+  cards.push({
+    src: `https://deckofcardsapi.com/static/img/${cardVals[i]}${suits[suitIndex]}.png`,
+    id:  `${cardVals[i]}${suits[suitIndex]}`
+  })
+}
 
-// let cardVals = ['A','K','Q','J','2','3','4','5','6','7','8','9','0']
-// let suits = ['C','H','D','S']
-
-// loopedCards = []
-
-// let suitIndex = 0
-// for (let i = 0; i < 13; i++){
-//   suitIndex++
-//   suitIndex = i % 4 === 0 ? 0 : suitIndex
-//   loopedCards.push({
-//     src: `https://deckofcardsapi.com/static/img/${cardVals[i]}${suits[suitIndex]}.png`,
-//     id:  `${cardVals[i]}${suits[suitIndex]}`
-//   })
-// }
+// THank you to AlexJWayne for an an accurate way to log time without drifting. https://gist.github.com/AlexJWayne/1431195
+// Slightly modified to accept 'normal' interval/timeout format (func, time).
+window.accurateInterval = function(fn, time) {
+  var cancel, nextAt, timeout, wrapper;
+  nextAt = new Date().getTime() + time;
+  timeout = null;
+  wrapper = function() {
+    nextAt += time;
+    timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+    return fn();
+  };
+  cancel = function() {
+    return clearTimeout(timeout);
+  };
+  timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+  return {
+    cancel: cancel
+  };
+};
 
 let clickedCards = []
 let matchCount = 0
 let cardFlips = 0
 let cardsPlayed = 0
+let timeElapsed = 0
+let timer
 
+function startStop() {
+  if (!timer){
+    timer = accurateInterval(updateTimer, 1000);
+  } else {
+    console.log('no timer')
+    timer.cancel()
+    timer = ''
+  }
+}
+
+function updateTimer () {
+  timeElapsed++
+  let mins = Math.floor(timeElapsed / 60)
+  let secs = timeElapsed - mins * 60
+  mins = mins < 10 ? '0' + mins : mins
+  secs = secs < 10 ? '0' + secs: secs
+  timerDisplay.innerText = `Time: ${mins}:${secs}`
+}
+
+function updateScore(){
+  cardFlips++
+  scoreDisplay.innerText = `Cards flipped: ${cardFlips}`
+}
 
 function shuffle(array) {
   let counter = array.length;
@@ -60,7 +78,6 @@ function shuffle(array) {
     array[counter] = array[index];
     array[index] = temp;
   }
-
   return array;
 }
 
@@ -72,6 +89,7 @@ function createCards(cardArray) {
     newImg.id = card.id
     newImg.addEventListener("click", handleCardClick);
     gameContainer.append(newImg);
+
   }
 }
 function checkMatches () {
@@ -81,31 +99,29 @@ function checkMatches () {
     matchCount++
   }  else {
     cardOne.src = cardBack
+    cardOne.classList.toggle('flip');
     cardOne.addEventListener("click", handleCardClick);
     cardTwo.src = cardBack
+    cardTwo.classList.toggle('flip');
     cardTwo.addEventListener("click", handleCardClick);
   }
   clickedCards = []
   if (matchCount === cardsPlayed / 2){
+    startStop()
     alert('Congrats! You matched all the cards!')
   }
 }
-function updateScore(){
-  cardFlips++
-  score.innerText = `Cards flipped: ${cardFlips}`
-
-}
-
 function handleCardClick(event) {
-  console.log('clicked')
   if (clickedCards.length === 2){
     return
   }
   else {
-    const found = cards.find(element => element.id === event.target.id);
-    event.target.src = found.src
-    event.target.removeEventListener("click", handleCardClick);
-    clickedCards.push(event.target)
+    let card = event.target
+    card.classList.toggle('flip');
+    const found = cards.find(element => element.id === card.id);
+    card.src = found.src
+    card.removeEventListener("click", handleCardClick);
+    clickedCards.push(card)
     updateScore()
     if (clickedCards.length === 2){
       setTimeout(checkMatches, 1000)  
@@ -118,17 +134,25 @@ function removeAllChildNodes(parent) {
       parent.removeChild(parent.firstChild);
   }
 }
-
+// func newGame stops current game if one exists, or creates a new game with num number of cards.
 function newGame(num){
   const oldCards = document.getElementsByClassName("cards");
   if (oldCards.length !== 0){
+    if (timer){
+      timer.cancel()
+      timer = ''
+    }
     matchCount = 0
     cardFlips = 0
-    score.innerText = "Cards flipped: 0"
+    timeElapsed = 0
+    scoreDisplay.innerText = "Cards flipped: 0"
+    timerDisplay.innerText = "Time: 00:00"
     removeAllChildNodes(gameContainer)
+  } 
+    cardsPlayed = num
+    let arr = cards.slice(0, num)
+    let shuffledCards = shuffle(arr);
+    createCards(shuffledCards);
+    startStop()
   }
-  cardsPlayed = num
-  let arr = cards.slice(0, num)
-  let shuffledCards = shuffle(arr);
-  createCards(shuffledCards);
-}
+
